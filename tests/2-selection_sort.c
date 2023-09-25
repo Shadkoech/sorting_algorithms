3ae435ddbda605a27abef8bac6d54997d9c6b15b
kibuchijw <kibuchijw@gmail.com>
2023-09-25 08:13:19 +0000

    Added Selection sort implementation

---
+++ tests/2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts array of integers in ascending order
+ * 		    Uses Selection sort algorithm
+ * @array: Array to sort
+ * @size: Number of elements in the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t min_index, i, j, k;
+	if (!array || size <= 1)
+		return;
+	
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+		
+		/* Find index of minimum element in unsorted part */
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+			{
+				min_index = j;
+			}
+		}
+		/* Swap current element with minimum element */
+		if (min_index != i)
+		{
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min-index] = temp;
+
+			/* Print array after each swap */
+			for (k = 0; k < size - 1; k++)
+			{
+				printf("%d, ", array[k]);
+			}
+			printf("%d\n", array[size - 1]);
+		}
+	}
+}
